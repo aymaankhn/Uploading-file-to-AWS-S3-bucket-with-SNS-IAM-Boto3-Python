c2b4b89f154676ec6ff3b4400de31b16f250868b
aymaankhn <101939816+aymaankhn@users.noreply.github.com>
2023-09-24 21:21:46 +0530

    Add files via upload

---
+++ aws1_S3.py
@@ -0,0 +1,35 @@
+import pandas as pd 
+import boto3
+from io import StringIO
+
+data = [["AA",1],["BB",2],['CC',3]]
+df = pd.DataFrame(data, columns=['Name','Roll.No'])
+
+Access_key1= "your_access_key"
+Secret_key1 = "your_secret_key"
+
+def upload_to_s3(df):
+    f = "file.csv"
+    s3 = boto3.client('s3',aws_access_key_id = Access_key1, aws_secret_access_key = Secret_key1) # Creating S3 Client
+    
+    csv_buf = StringIO() # Convert dataframe to csv file                                          
+    df.to_csv(csv_buf, header=True, index=False)
+    csv_buf.seek(0)
+
+    # Uploading file to s3
+    s3.put_object(Bucket = 'aymaan-bucket', Body = csv_buf.getvalue(), Key = 'MyFolder/' + f)
+
+# Creating SNS 
+ses = boto3.client('sns', aws_access_key_id = Access_key1, aws_secret_access_key = Secret_key1, region_name='ap-south-1')
+sns_topicname_arn = "arn:aws:sns:ap-south-1:912624467490:upload_notification"
+
+def Message(snsArn, msg):
+    client = boto3.client('sns', aws_access_key_id = Access_key1, aws_secret_access_key =Secret_key1,region_name='ap-south-1')
+    client.publish(TargetArn=snsArn, Message=msg)
+
+
+upload_to_s3(df)
+
+msg = "Hey! File has been uploaded into your bucket\n Regards Aymaan"
+
+Message(sns_topicname_arn, msg)
